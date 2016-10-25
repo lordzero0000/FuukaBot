dafa850b6e0b0bd13da79672a7b2058ae183e195
Iuk <iukchido70@gmail.com>
2016-10-24 20:34:35 -0600

    Más respuestas y mejores cosas.

--- bot.js
+++ bot.js
@@ -13,12 +13,19 @@ var responseObject = {
   "Yare yare": "Desu.",
   "yare, yare.": "Desu.",
   "yare yare": "Desu.",
-  "Hoo!": "Haa!",
+  "Hoo!": "Ha!",
   "Say my name.": "You're Heisenberg...",
   "Nah": "Nah, son.",
   "What is love?": "Baby, don't hurt me.",
   "fierro": "¡No fierro!",
   "Fuuka you": "No.",
+  "Sal": ":PJsalt:",
+  "Salt": ":PJsalt:",
+  "Hoo ha!": "http://i1.kym-cdn.com/photos/images/newsfeed/000/914/507/f3f.png",
+  "How do you turn this on?": "http://vignette3.wikia.nocookie.net/ageofempires/images/7/78/Image.jpg",
+  "No emoji!": "https://cdn.discordapp.com/attachments/140563979498946561/224044914894176257/No_emoji.jpg",
+  "alv": "¿Alvarito?",
+  ":(": ":)",
   "ayy": "Ayy, lmao!",
   "wat": "Say what?",
   "lol": "roflmaotntpmp",
@@ -42,6 +49,9 @@ client.on('message', message => {
   if (message.content === ":'v") {
     message.reply('me has decepcionado...');
   }
+  if (message.content === "meme") {
+    message.reply('yamete!');
+  }
   if (message.content === '¿Qué avatar tengo?') {
     // send the user's avatar URL
     message.reply(message.author.avatarURL);
@@ -55,10 +65,10 @@ client.on('message', message => {
   if(!message.content.startsWith(prefix)) return;
 
   if (message.content.startsWith(prefix + "ayuda")) {
-    message.channel.sendMessage('Estos son los comandos actuales:\n"!ayuda": Muestra esta ayuda.\n"!8ball" + pregunta: Trataré de responder a tus preguntas. Ten en cuenta que la respuesta sólo pueder ser sí o no. Por favor, formula tus preguntas adecuadamente.\n"!moneda": Lanzaré una moneda al aire por ti y te diré el resultado.\n"!dado6": Tiraré un dado de seis caras.');
+    message.channel.sendMessage('Estos son los comandos actuales:\n"!ayuda": Muestra esta ayuda.\n"!8ball" + pregunta: Trataré de responder a tus preguntas. Ten en cuenta que la respuesta sólo pueder ser sí o no. Por favor, formula tus preguntas adecuadamente.\n"!moneda": Lanzaré una moneda al aire por ti y te diré el resultado.\n"!dado6": Tiraré un dado de seis caras.\n"¿Qué avatar tengo?": Mostraré tu avatar a todos.');
   }
   if (message.content.startsWith(prefix + "otp")) {
-    message.channel.sendMessage("Eres mi vida, ¡te amo, Poni! ❤");
+    message.channel.sendMessage("Poni... *blush* Te amo... :two_hearts:");
   }
 });
 
@@ -87,7 +97,14 @@ client.on('message', message => {
 	r_text[14] = "*blush* Sí...";
 	r_text[15] = "¡N-No, ¿cómo crees?!";
 	r_text[16] = "*gasp* ¡No me preguntes eso...!";
-	var i = Math.floor(14*Math.random())
+	r_text[17] = "¡Ni en un millón de años!";
+	r_text[18] = "Ah... no.";
+	r_text[19] = "Parece que no.";
+	r_text[20] = "Si tienes fe, sí.";
+	r_text[21] = "¡Sí!";
+	r_text[22] = "Todo indica que sí.";
+	r_text[23] = "El futuro lo decidimos nosotros.";
+	var i = Math.floor(24*Math.random())
 	var i = r_text[i];
     message.channel.sendMessage(i);
   }
@@ -127,6 +144,17 @@ client.on('message', message => {
   }
 });
 
+client.on('message', message => {  
+  if (message.content === "Life is...") {
+	var r_text = new Array ();
+	r_text[0] = "Strange...";
+	r_text[1] = "Weird...";
+	r_text[2] = "Simply unfair. Don't you think?";
+	var i = Math.floor(3*Math.random())
+	var i = r_text[i];
+    message.channel.sendMessage(i);
+  }
+});
 
 client.on("guildMemberAdd", (guild, member) => {
     console.log(`New User "${member.user.username}" has joined "${guild.name}"` );
