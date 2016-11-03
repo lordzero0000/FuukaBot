81e9db7438c431bfbad7745ce8d7cf3a35a2c0b3
Iuk <iukchido70@gmail.com>
2016-11-02 19:33:38 -0700

    Gags y más chorradas

--- bot.js
+++ bot.js
@@ -25,6 +25,7 @@ var responseObject = {
   "Yare yare": "Desu.",
   "yare, yare.": "Desu.",
   "yare yare": "Desu.",
+  "JoJo": "Sono chi no sadame!",
   "Hoo!": "Ha!",
   "Say my name.": "You're Heisenberg...",
   "Nah": "Nah, son.",
@@ -36,14 +37,18 @@ var responseObject = {
   "Hoo ha!": "http://i1.kym-cdn.com/photos/images/newsfeed/000/914/507/f3f.png",
   "How do you turn this on?": "http://vignette3.wikia.nocookie.net/ageofempires/images/7/78/Image.jpg",
   "No emoji!": "https://cdn.discordapp.com/attachments/140563979498946561/224044914894176257/No_emoji.jpg",
+  "rofl": "http://i2.kym-cdn.com/photos/images/original/000/296/199/9ff.gif",
   "alv": "¿Alvarito?",
   ":(": ":)",
   "ayy": "Ayy, lmao!",
   "wat": "Say what?",
   "lol": "roflmaotntpmp",
-  "Poni": "❤",
-  "Pony": "❤",
-  "pony": "❤"
+  ":pikachu": "X",
+  ":PJsalt:": "X",
+  ":heart": "X"
+ // "Poni": "❤",
+ // "Pony": "❤",
+ // "pony": "❤"
 };
 client.on('message', (message) => {
   if(responseObject[message.content]) {
@@ -75,12 +80,12 @@ client.on('message', message => {
     let prefix = "!";
   // Exit and stop if it's not there
   if(!message.content.startsWith(prefix)) return;
-
+  
   if (message.content.startsWith(prefix + "ayuda")) {
-    message.channel.sendMessage('Estos son los comandos actuales:\n"!ayuda": Muestra esta ayuda.\n"!8ball" + pregunta: Trataré de responder a tus preguntas. Ten en cuenta que la respuesta sólo pueder ser sí o no. Por favor, formula tus preguntas adecuadamente.\n"!moneda": Lanzaré una moneda al aire por ti y te diré el resultado.\n"!dado6": Tiraré un dado de seis caras.\n"¿Qué avatar tengo?": Mostraré tu avatar a todos.');
+    message.channel.sendMessage('Estos son los comandos actuales:\n"!ayuda": Muestra esta ayuda.\n"!8ball" + pregunta: Trataré de responder a tus preguntas. Ten en cuenta que la respuesta sólo pueder ser sí o no. Por favor, formula tus preguntas adecuadamente.\n"!moneda": Lanzaré una moneda al aire por ti y te diré el resultado.\n"!dado6": Tiraré un dado de seis caras.\n""!quien"" + pregunta: Desde quíén es casual a quién banear. Te diré a quién buscas.\n""!joke": Contaré un chiste. Trataré de que sea uno bueno.\n"¿Qué avatar tengo?": Mostraré tu avatar a todos.');
   }
   if (message.content.startsWith(prefix + "otp")) {
-    message.channel.sendMessage("Poni... *blush* Te amo... :two_hearts:");
+    message.channel.sendMessage("... *sob*");
   }
 });
 
@@ -89,7 +94,7 @@ client.on('message', message => {
     let prefix = "!";
   // Exit and stop if it's not there
   if(!message.content.startsWith(prefix)) return;
-
+  
   if (message.content.startsWith(prefix + "8ball")) {
 	var r_text = new Array ();
 	r_text[0] = "¡Ni lo dudes!";
@@ -126,7 +131,31 @@ client.on('message', message => {
     let prefix = "!";
   // Exit and stop if it's not there
   if(!message.content.startsWith(prefix)) return;
+  
+  if (message.content.startsWith(prefix + "quien")) {
+	var r_text = new Array ();
+	r_text[0] = "Vito.";
+	r_text[1] = "Unipax.";
+	r_text[2] = "Psicomental.";
+	r_text[3] = "Christian.";
+	r_text[4] = "Diego.";
+	r_text[5] = "Iuk.";
+	r_text[6] = "Aldair.";
+	r_text[7] = "Zero.";
+	r_text[8] = "Darmer.";
+	r_text[9] = "Volkner.";
+	r_text[10] = "El Poni.";
+	var i = Math.floor(11*Math.random())
+	var i = r_text[i];
+    message.channel.sendMessage(i);
+  }
+});
 
+client.on('message', message => {
+    let prefix = "!";
+  // Exit and stop if it's not there
+  if(!message.content.startsWith(prefix)) return;
+  
   if (message.content.startsWith(prefix + "moneda")) {
 	var r_text = new Array ();
 	r_text[0] = "Cayó cara.";
@@ -141,7 +170,7 @@ client.on('message', message => {
     let prefix = "!";
   // Exit and stop if it's not there
   if(!message.content.startsWith(prefix)) return;
-
+  
   if (message.content.startsWith(prefix + "dado6")) {
 	var r_text = new Array ();
 	r_text[0] = "1, como el as.";
@@ -165,6 +194,71 @@ client.on('message', message => {
 	var i = Math.floor(3*Math.random())
 	var i = r_text[i];
     message.channel.sendMessage(i);
+  }
+});
+
+client.on('message', message => {
+    let prefix = "!";
+  // Exit and stop if it's not there
+  if(!message.content.startsWith(prefix)) return;
+  
+  if (message.content.startsWith(prefix + "joke")) {
+	var r_text = new Array ();
+	r_text[0] = 'Un orco entra a un bar con un perico en su hombro. El cantinero pregunta "Hey, ¿de dónde sacaste eso?" Y el perico le contesta "En Durotar. Ahí hay muchos".';
+	r_text[1] = "¿Cómo se oculta un Tauren en un árbol de cerezo? Se pinta las pezuñas de rojo.";
+	r_text[2] = 'Un tipo se me acercó y dijo "¡Soy un arco! ¡Soy una ballesta! ¡Soy un arco!", y yo le contesté "¡Relájate! Estás muy tenso".';
+	r_text[3] = 'Tengo una gran idea para una película. Trata de dos gnomos que encuentran un brazalete de poder. Tienen que llevarlo a las Estepas Ardientes y arrojarlo a la Caldera. Forman la Hermandad del Brazalete. Durante el camino los sigue un múrloc llamado Gottom, quien está obsesionado con el brazalete. También nueve Hombres del Saco que se lo quieren robar. Podría ser una trilogía llamada "El Soberano del Brazalete". La primera parte podría llamarse "La Hermandad del Brazalete", seguida de "Un par de torres", con el culminante final "¡Ey! ¡Volvió el rey!".';
+	r_text[4] = "¿Por qué todos suponen que sé coser y cocinar?";
+	r_text[5] = "¿Alguna vez te sientes como si no estuvieras al mando de tu propio destino? Como... ¿si te controlara una mano invisible?";
+	r_text[6] = "A veces tengo problemas para controLAR ¡EL VOLUMEN DE MI VOZ!";
+	r_text[7] = "No me molestan los gnomos, pero me preocupa tropezarme con uno.";
+	r_text[8] = "Exactamente, ¿cómo se estrella alguien contra un planeta? Eso me gustaría saber.";
+	r_text[9] = '"Detente y pregunta dónde estamos", le dije. Pero no, "Es inter-dimensional", me dijo. "¿Qué puede salir mal?"';
+	r_text[10] = "Me sé una receta estupenda. Necesitas dos gnomos y dos huevos. Parte los gnomos y separa los huevos... o... ¿cómo era? Detalles...";
+	r_text[11] = "Lo tenemos todo planeado. Paso uno: aterrizamos el Exodar. Paso tres: derrotamos a la Legión y volvemos a casa... Sólo falta un detalle.";
+	r_text[12] = 'No nos dimos cuenta, pero en el idioma de los Naaru, "Exodar" significa "popó de elekk defectuosa".';
+	r_text[13] = "*snif* Mmmm, eso huele a... *snif* ¿tocino? *snif* Oigan, huele a... *snif* ¿Huelen el tocino? ¡¿Alguien?! *snif* ¡Oh, sí es tocino! ¡Tocino! ¡¿Quién tiene el tocino?! ¡¿DÓNDE ESTÁ EL TOCINO?!";
+	r_text[14] = "Mis más sinceras disculpas por cualquier inconveniente que mi locura asesina pudo haber causado.";
+	r_text[15] = "Descubrí que ser golpeada por un arma contundente puede ser bastante doloroso.";
+	r_text[16] = "Las ardillas pueden ser letales cuando son acorraladas... ¿sabes?";
+	r_text[17] = "Espero algún día encontrarle los nuggets a un pollo.";
+	r_text[18] = "Tuve una idea para un aparato que cocina pedazos pequeños de pan, pero la descarté porque no creo que haya mercado para eso.";
+	r_text[19] = "Me siento muy femenina, ¡y le romperé la cara a quien no esté de acuerdo!";
+	r_text[20] = "Si te pones entre mi comida y yo, perderás una mano.";
+	r_text[21] = "No me gusta la Cima del Trueno. No venden hamburguesas en ninguna parte.";
+	r_text[22] = 'Fui a Entrañas a hacerme un facial. ¡Ja! ¿Has visto a esa gente? Le dije "Ni siquiera tienes quijada, ¿y vas a hacerme un facial?". Se enojó... o eso creo. ¿Has escuchado hablar a alguien sin quijada? "Rau, au, ueue". ¡Ja, ja! ¡Parecía un múrloc!.';
+	r_text[23] = "¿Cómo te voy a hechar de menos si no te largas?";
+	r_text[24] = "Los espejos no hablan. Para tu suerte, ¡tampoco se ríen!";
+	r_text[25] = "Si no tienes éxito a la primera, vuelve a hacerlo estallar.";
+	r_text[26] = "¡Oh, se me ocurrió algo! ¿Y si organizáramos el crimen? Sí...";
+	r_text[27] = "Ante la duda... C4.";
+	r_text[28] = "Veamos... eh... adelante, abajo, adelante, ¡golpe! No... ¿abajo, arriba, patada?";
+	r_text[29] = "Soy vegetariana. Sólo como plantas. Y animales que comen plantas.";
+	r_text[30] = "¡La fruta es prueba de que los dioses existen y nos aman! Es broma, la vida es un chiste.";
+	r_text[31] = "Tu vida. :)";
+	r_text[32] = "¿Y tu Skell?";
+	r_text[33] = "Tastu: Cuando Tatsu se marchaba, Mamapon siempre usaba comida para traerlo de vuelta.\nLin: Sí, los nopon y los gatos parecen tener el mismo nivel de inteligencia...";
+	r_text[34] = "¿Sabes...? Normalmente no le muestro esto a nadie, pero he estado trabajando en un fanfic de Skells... Dame un segundo...";
+	r_text[35] = "Tatsu sabe cómo te sientes. No tiene que avergonzarte ser un bulto ineficiente e inútil.";
+	r_text[36] = "Eres una patata dura. Me gustan las patatas duras. No me obligues a hacer puré de patata...";
+	r_text[37] = "¡Tatsu no dejará que nadie se meta con Mamapon! ¡Quien lo intente se enfrentará a la furia de... los confiables amigos de Tatsu!";
+	r_text[38] = "¿Mmm? ¿Que si no salgo? Bueno, cuando hay una computadora, normalmente me quedo en casa.";
+	r_text[39] = "Seven: Gimme a P!\nClover: Gimme an I!\nSnake: Gimme a P and an E!\nJunpei: What's that spell? Pipe!\n..\n..\nWhat the hell are we doing...";
+	var i = Math.floor(40*Math.random())
+	var i = r_text[i];
+    message.channel.sendMessage(i);
+  }
+});
+
+client.on('message', message => {  
+  if (message.content.startsWith("https://discord.gg/")) {
+    message.reply("¡no nohrios!");
+  }
+});
+
+client.on('message', message => {
+  if (message.content === (':ok_hand:')) {
+    message.reply("me has decepcionado.\n¡Necesito un monje! @Iuk#3771");
   }
 });
 
